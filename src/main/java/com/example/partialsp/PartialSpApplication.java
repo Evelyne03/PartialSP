97f18018c9c602093ce7814fb64ad94efbbe7e4d
Tudureanu Evelyne-Andreea <evelyne.tudureanu03@e-uvt.ro>
2023-11-23 09:55:48 +0200

    Initial commit

---
+++ src/main/java/com/example/partialsp/PartialSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.partialsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartialSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartialSpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/partialsp/PartialSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.partialsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartialSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
